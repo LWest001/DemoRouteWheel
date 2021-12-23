66a538f7f49ec8d4f8e1538c6c7a0997297a4c87
LWest001 <93659375+LWest001@users.noreply.github.com>
2021-12-23 12:52:09 -0500

    timer for breaks
    Removed timer.js from the script - now it is an inline function that is called when the first 2 breaks are loaded and locks out the proceed button until 3 minutes pass. On the third break, the user may simply save data and move on, no timer.

--- timer.js
+++ timer.js
@@ -1,13 +1,15 @@
-var sec = 0;
+function startClock() {
+    var sec = 0;
 
-function pad(val) { return val > 9 ? val : "0" + val; }
-setInterval(function() {
-    document.getElementById("seconds").innerHTML = pad(++sec % 60);
-    document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
-}, 1000);
+    function pad(val) { return val > 9 ? val : "0" + val; }
+    setInterval(function() {
+        document.getElementById("seconds").innerHTML = pad(++sec % 60);
+        document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
+    }, 1000);
 
-function changeColor() {
-    document.getElementById("clock").style.color = "red";
-    document.getElementById("breakPrompt").innerHTML = "Please press the button now to proceed to the next block.";
+    function changeColor() {
+        document.getElementById("clock").style.color = "red";
+        document.getElementById("breakPrompt").innerHTML = "Please press the button now to proceed to the next block.";
+    }
+    setInterval(changeColor, 180000);
 }
-setInterval(changeColor, 180000);
