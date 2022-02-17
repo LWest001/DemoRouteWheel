54275cb6ff55828d787af53285eef7bcf84c1579
LWest001 <93659375+LWest001@users.noreply.github.com>
2022-02-17 14:33:29 -0500

    countdown refactor
    refactor countdown clock into if else

--- make_condition_sb1_360.js
+++ make_condition_sb1_360.js
@@ -39,9 +39,9 @@ function make_condition(radius, n_bin_onWheel, n_repetition, n_block, img_host, 
                 basic_condition.push({
                     radius: radius[r],
                     scene_num: ('00000' + (bin_startPoint[b] + shuffled_jitter[rp])).slice(-6).toString(),
-                    wheel_path: img_host + "SB1_360/", //Wheel path is the whole folder, so it the image can change base on the mouse, by Bill 11/23/2021 // change to IS2 by Leo 12/2/2021
+                    wheel_path: img_host + "SB1_360/", //Wheel path is the whole folder, so it the image can change base on the mouse, by Bill 11/23/2021
                     //wheel_path: img_host+'Wheel'+wheel_num+'/wheel'+wheel_num+'_r'+radius[r]+'/', //Wheel path 
-                    img_path: img_host + "SB1_360/" + ('00000' + (bin_startPoint[b] + shuffled_jitter[rp])).slice(-6) + '.jpg', // img_path is the image for the target, change it to ISC, by Bill 11/23/2021// change to IS2 by Leo 12/2/2021
+                    img_path: img_host + "SB1_360/" + ('00000' + (bin_startPoint[b] + shuffled_jitter[rp])).slice(-6) + '.jpg', // img_path is the image for the target, change it to ISC, by Bill 11/23/2021
                     // img_path: img_host+'Wheel'+wheel_num+'/wheel'+wheel_num+'_r'+radius[r]+'/'+
                     //         ('00000'+(bin_startPoint[b]+shuffled_jitter[rp])).slice(-6)+'.webp'   
                     target_series: targetSeries
